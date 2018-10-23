53b30eabb6aa9b82846057dbf12619272567a934
Rolf van de Krol <info@rolfvandekrol.nl>
2018-10-23 21:28:49 +0200

    Fix call to LanguageManager

--- src/Normalizer/EntityReferenceFieldItemTargetNormalizer.php
+++ src/Normalizer/EntityReferenceFieldItemTargetNormalizer.php
@@ -44,7 +44,7 @@ class EntityReferenceFieldItemTargetNormalizer extends EntityReferenceFieldItemN
   public function normalize($field_item, $format = NULL, array $context = []) {
     $values = parent::normalize($field_item, $format, $context);
 
-    $langcode = $this->languageManager->getCurrentLanguage(LanguageInterface::TYPE_CONTENT);
+    $langcode = $this->languageManager->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)->getId();
 
     /** @var \Drupal\Core\Entity\EntityInterface $entity */
     if ($entity = $field_item->get('entity')->getValue()) {
