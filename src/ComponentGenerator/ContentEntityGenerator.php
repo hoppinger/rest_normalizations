0331357b3ab3842bfc5996e1880f80c621276929
Rolf van de Krol <info@rolfvandekrol.nl>
2018-11-11 20:00:54 +0100

    Add entity operations logic

--- src/ComponentGenerator/ContentEntityGenerator.php
+++ src/ComponentGenerator/ContentEntityGenerator.php
@@ -45,11 +45,83 @@ class ContentEntityGenerator extends EntityGenerator {
     ]);
   }
 
+  protected function getOperationProperties(Settings $settings, Result $result, ComponentResult $componentResult) {
+    return [
+      'title' => 'string',
+      'url' => 'string',
+      'weight' => 'number'
+    ];
+  }
+
+  protected function getOperationMapping($properties, Settings $settings, Result $result, ComponentResult $componentResult) {
+    return ['title', 'url'];
+  }
+
+  protected function generateOperation(Settings $settings, Result $result, ComponentResult $componentResult) {
+    $properties = $this->getOperationProperties($settings, $result, $componentResult);
+
+    return $this->generatePropertiesComponentResult(
+      $properties,
+      'EntityOperation',
+      'ParsedEntityOperation',
+      'entity_operation_parser',
+      $this->getOperationMapping($properties, $settings, $result, $componentResult),
+      $settings,
+      $result
+    );
+  }
+
+  protected function generateOperations(Settings $settings, Result $result, ComponentResult $componentResult) {
+    $operation = $this->generateOperation($settings, $result, $componentResult);
+
+    $type = $result->setComponent(
+      'types/EntityOperations', 
+      'interface EntityOperations { [key: string]: ' . $operation->getComponent('type') . '}'
+    );
+    $target_type = $result->setComponent(
+      'types/ParsedEntityOperations',
+      'type ParsedEntityOperations = ' . 
+        ':/immutable/OrderedMap:<string, ' . $operation->getComponent('target_type') . '>'
+    );
+    $parser = $result->setComponent(
+      'parser/entity_operations_parser', 
+      'const entity_operations_parser = ' . 
+        '(l: ' . $type . ' | undefined): ' . $target_type . ' => '. 
+          'l ? :/immutable/OrderedMap:<string, ' . $operation->getComponent('type') . '>(l).sortBy(o => (o as ' . $operation->getComponent('type') . ').weight).map(o => ' . $operation->getComponent('parser') . '(o as ' . $operation->getComponent('type') . ')).toOrderedMap() : :/immutable/OrderedMap:<string, ' . $operation->getComponent('type') . '>()'
+    );
+
+    return new ComponentResult([
+      'type' => $type . ' | undefined',
+      'target_type' => $target_type,
+      'parser' => $parser,
+    ]);
+  }
+
   protected function getBaseProperties($object, Settings $settings, Result $result, ComponentResult $componentResult) {
     $properties = parent::getBaseProperties($object, $settings, $result, $componentResult);
 
-    $properties['language_links'] = $this->generateLanguageLinks($settings, $result, $componentResult);
+    $properties['language_links'] = $componentResult->getContext('language_links');
+    $properties['entity_operations'] = $componentResult->getContext('operations');
 
     return $properties;
+  }
+
+  protected function preGenerate($object, Settings $settings, Result $result, ComponentResult $componentResult) {
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $object */
+
+    // The things are used in the base properties, so need to be done before the entity preGenerate
+    $language_links = $componentResult->getContext('language_links');
+    if (!isset($language_links)) {
+      $language_links = $this->generateLanguageLinks($settings, $result, $componentResult);
+      $componentResult->setContext('language_links', $language_links);
+    }
+
+    $operations = $componentResult->getContext('operations');
+    if (!isset($operations)) {
+      $operations = $this->generateOperations($settings, $result, $componentResult);
+      $componentResult->setContext('operations', $operations);
+    }
+
+    parent::preGenerate($object, $settings, $result, $componentResult);
   }
 }
--- src/Normalizer/ContentEntityNormalizer.php
+++ src/Normalizer/ContentEntityNormalizer.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\rest_normalizations\Normalizer;
 
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\serialization\Normalizer\CacheableNormalizerInterface;
 use Drupal\serialization\Normalizer\ContentEntityNormalizer as BaseNormalizer;
 
 class ContentEntityNormalizer extends BaseNormalizer {
@@ -12,6 +14,22 @@ class ContentEntityNormalizer extends BaseNormalizer {
     $data['language_links'] = [];
     foreach ($object->getTranslationLanguages() as $language) {
       $data['language_links'][$language->getId()] = $object->getTranslation($language->getId())->url('canonical');
+    }
+
+    $currentUser = \Drupal::currentUser();
+
+    if ($currentUser->hasPermission('view entity operations in rest')) {
+      try {
+        $listBuilder = $this->entityManager->getListBuilder($object->getEntityTypeId());
+      } catch (InvalidPluginDefinitionException $e) {
+        return $data;
+      }
+      
+      $data['entity_operations'] = $listBuilder->getOperations($object);
+
+      if (isset($context[CacheableNormalizerInterface::SERIALIZATION_CONTEXT_CACHEABILITY])) {
+        $context[CacheableNormalizerInterface::SERIALIZATION_CONTEXT_CACHEABILITY]->addCacheContexts(['user']);
+      }
     }
 
     return $data;
