d011c7b7fdbb072b1aa9badc035a8eb8096fd96c
Amit Ajgaonkar <amit11041991@gmail.com>
2026-03-03 00:00:03 +0530

    Fix php warning

--- src/Normalizer/ContentEntityNormalizer.php
+++ src/Normalizer/ContentEntityNormalizer.php
@@ -16,7 +16,7 @@ class ContentEntityNormalizer extends BaseNormalizer {
    */
   protected $exclude_operations;
 
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, ?EntityTypeRepositoryInterface $entity_type_repository = NULL, ?EntityFieldManagerInterface $entity_field_manager = NULL, $exclude_operations) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ?EntityTypeRepositoryInterface $entity_type_repository = NULL, ?EntityFieldManagerInterface $entity_field_manager = NULL, array $exclude_operations = [] ) {
     parent::__construct($entity_type_manager, $entity_type_repository, $entity_field_manager);
 
     $this->exclude_operations = $exclude_operations;
